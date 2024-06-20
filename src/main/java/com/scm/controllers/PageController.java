aad5224a5978028350c71b5e601b2aef7c846545
richik96 <drichik@gmail.com>
2024-06-20 22:33:39 +0530

    1st commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,43 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home Page handler");
+
+        //sending data to view
+        model.addAttribute("name", "Richik");
+        model.addAttribute("githubRepo", "https://github.com/richik96");
+        
+        return "home";
+    }
+
+    //about route
+    @RequestMapping("/about")
+    public String aboutpage(Model model) {
+        System.out.println("About Page handler");
+
+        //sending data to view
+        model.addAttribute("name", "Richik");
+        return "about";
+    }
+
+    //services
+    @RequestMapping("/services")
+    public String servicespage(Model model) {
+        System.out.println("Services Page handler");
+
+        //sending data to view
+        model.addAttribute("name", "Richik");
+        return "services";
+    }
+}
