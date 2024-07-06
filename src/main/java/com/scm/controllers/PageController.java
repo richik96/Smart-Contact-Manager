6d714335fbeb5efd77936be8119b6b19636fdfd8
richik96 <drichik@gmail.com>
2024-07-06 18:09:51 +0530

    understanding fragents

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,8 +2,8 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 
